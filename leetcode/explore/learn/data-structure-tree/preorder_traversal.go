3479f167694d4342f95aeeea7e7a6eb51379c3d8
Yusuke Unno <nnno@nnno.jp>
2022-02-13 23:06:31 +0900

    一旦ここで保存

--- leetcode/explore/learn/data-structure-tree/preorder_traversal.go
+++ leetcode/explore/learn/data-structure-tree/preorder_traversal.go
@@ -6,6 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 再帰を用いた解き方
 func preorderTraversal(root *TreeNode) []int {
 	var ans []int
 	if root != nil {
@@ -18,5 +19,31 @@ func preorderTraversal(root *TreeNode) []int {
 			ans = append(ans, preorderTraversal(root.Right)...)
 		}
 	}
+	return ans
+}
+
+// 配列(slice)を用いた解き方
+func preorderTraversalStack(root *TreeNode) []int {
+	var ans []int
+
+	if root != nil {
+		var stack []*TreeNode
+		stack = append(stack, root)
+
+		for len(stack) != 0 {
+			lastNode := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			ans = append(ans, lastNode.Val)
+
+			if lastNode.Right != nil {
+				stack = append(stack, lastNode.Right)
+			}
+
+			if lastNode.Left != nil {
+				stack = append(stack, lastNode.Left)
+			}
+		}
+	}
+
 	return ans
 }
--- leetcode/explore/learn/data-structure-tree/preorder_traversal_test.go
+++ leetcode/explore/learn/data-structure-tree/preorder_traversal_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 )
 
-func Test_PreorderTraversal(t *testing.T) {
-	type testData struct {
-		name string
-		root *TreeNode
-		want []int
-	}
+type testData struct {
+	name string
+	root *TreeNode
+	want []int
+}
+
+func makeTestData() []*testData {
 	var tests []*testData
 
 	//-- test 1
@@ -46,11 +47,28 @@ func Test_PreorderTraversal(t *testing.T) {
 	}
 	tests = append(tests, t3)
 
+	return tests
+}
+
+func Test_PreorderTraversal(t *testing.T) {
+	tests := makeTestData()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := preorderTraversal(tt.root)
 			if !reflect.DeepEqual(tt.want, got) {
 				t.Errorf("preorderTraversal want(%v) : actual(%v)", tt.want, got)
+			}
+		})
+	}
+}
+
+func Test_PreorderTraversalStack(t *testing.T) {
+	tests := makeTestData()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := preorderTraversalStack(tt.root)
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("preorderTraversalStack want(%v) : actual(%v)", tt.want, got)
 			}
 		})
 	}
